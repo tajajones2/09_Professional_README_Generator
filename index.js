29ca921a1a53eed04f594249981bf8917460e9b1
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-09-29 00:18:57 -0400

    created gitignore file and added utils folder and markdown file

---
+++ index.js
@@ -0,0 +1,5 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+var title = "";
